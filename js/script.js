5941914b06bec914b700df2ff272fec716d517d7
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-07 16:29:46 +0100

    add const variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert('ciao');
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+const nomeUtente;
+const cognomeUtente;
+const coloreUtente;
+
+const nomeUtente = prompt('Qual è il tuo nome?');
+const cognomeUtente = prompt('Qual è il tuo cognome?');
+const coloreUtente = prompt('Qual è il tuo colore preferito?');
