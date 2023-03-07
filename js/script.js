14710b5ba503969df5b09a0f9d657b1600938881
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-07 17:32:28 +0100

    add h1 w/ innerHTML

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 
-const nomeUtente;
-const cognomeUtente;
-const coloreUtente;
-
 const nomeUtente = prompt('Qual è il tuo nome?');
 const cognomeUtente = prompt('Qual è il tuo cognome?');
 const coloreUtente = prompt('Qual è il tuo colore preferito?');
+
+let html = document.getElementById("title").innerHTML;
